46de7e29cbbbe7e7dbde25d74528614a4641aa9a
mellablisstech <134699346+mellablisstech@users.noreply.github.com>
2024-01-09 04:38:40 -0800

    update

--- src/Components/CoursesSection.js
+++ src/Components/CoursesSection.js
@@ -4,9 +4,250 @@ function CoursesSection() {
   return (
     <>
       <p>
-        kgkugkugkug
+        <div className="hero-courses">
+          <div className="heading-line"></div>
+          <div className="sub-heading">
+            <div className="left">
+              <h1>Popular Courses</h1>
+            </div>
+            <div className="right">
+              <h1>EXPLORE COURSES</h1>
+              <i class="fa-solid fa-arrow-right"></i>
+            </div>
+          </div>
+          <div className="first-section">
+            <div className="first-a">
+              <div className="top1">
+                <div className="amount">
+                  <h1>₦50,000</h1>
+                </div>
+              </div>
+
+              <div className="bottom1">
+                <div className="writeupone">
+                  <div>
+                    <h1>Front-End development</h1>
+                  </div>
+                  <div className="star">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star differ"></i>
+                  </div>
+                </div>
+                <h1 className="note">
+                  Choosing Learn stack for your tech education means embarking
+                  on a transformation learning journey tailored to modern
+                </h1>
+                <div className="last">
+                  <div className="duration">
+                    <i class="fa-regular fa-clock font-a"></i>
+                    <div className="weeks">2 Weeks</div>
+                  </div>
+                  <div className="lesson">
+                    <i class="fa-regular fa-rectangle-list font-b"></i>
+                    <h1 className="fifteen">15 Lesson</h1>
+                  </div>
+
+                  <button className="btn">Enroll Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="first-b">
+              <div className="top2">
+                <div className="amount">
+                  <h1>₦50,000</h1>
+                </div>
+              </div>
+              <div className="bottom2">
+                <div className="writeupone">
+                  <div>
+                    <h1>Front-End development</h1>
+                  </div>
+                  <div className="star">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star differ"></i>
+                  </div>
+                </div>
+                <h1 className="note">
+                  Choosing Learn stack for your tech education means embarking
+                  on a transformation learning journey tailored to modern
+                </h1>
+                <div className="last">
+                  <div className="duration">
+                    <i class="fa-regular fa-clock font-a"></i>
+                    <div className="weeks">2 Weeks</div>
+                  </div>
+                  <div className="lesson">
+                    <i class="fa-regular fa-rectangle-list font-b"></i>
+                    <h1 className="fifteen">15 Lesson</h1>
+                  </div>
+
+                  <button className="btn">Enroll Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="first-c">
+              <div className="top3">
+                <div className="amount">
+                  <h1>₦50,000</h1>
+                </div>
+              </div>
+              <div className="bottom3">
+                <div className="writeupone">
+                  <div>
+                    <h1>Front-End development</h1>
+                  </div>
+                  <div className="star">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star differ"></i>
+                  </div>
+                </div>
+                <h1 className="note">
+                  Choosing Learn stack for your tech education means embarking
+                  on a transformation learning journey tailored to modern
+                </h1>
+                <div className="last">
+                  <div className="duration">
+                    <i class="fa-regular fa-clock font-a"></i>
+                    <div className="weeks">2 Weeks</div>
+                  </div>
+                  <div className="lesson">
+                    <i class="fa-regular fa-rectangle-list font-b"></i>
+                    <h1 className="fifteen">15 Lesson</h1>
+                  </div>
+
+                  <button className="btn">Enroll Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="second-section">
+            <div className="second-a">
+              <div className="top1">
+                <div className="amount">
+                  <h1>₦50,000</h1>
+                </div>
+              </div>
+
+              <div className="bottom1">
+                <div className="writeupone">
+                  <div>
+                    <h1>Front-End development</h1>
+                  </div>
+                  <div className="star">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star differ"></i>
+                  </div>
+                </div>
+                <h1 className="note">
+                  Choosing Learn stack for your tech education means embarking
+                  on a transformation learning journey tailored to modern
+                </h1>
+                <div className="last">
+                  <div className="duration">
+                    <i class="fa-regular fa-clock font-a"></i>
+                    <div className="weeks">2 Weeks</div>
+                  </div>
+                  <div className="lesson">
+                    <i class="fa-regular fa-rectangle-list font-b"></i>
+                    <h1 className="fifteen">15 Lesson</h1>
+                  </div>
+
+                  <button className="btn">Enroll Now</button>
+                </div>
+              </div>
+            </div>
+
+            <div className="second-b">
+              <div className="top1">
+                <div className="amount">
+                  <h1>₦50,000</h1>
+                </div>
+              </div>
+
+              <div className="bottom1">
+                <div className="writeupone">
+                  <div>
+                    <h1>Front-End development</h1>
+                  </div>
+                  <div className="star">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star differ"></i>
+                  </div>
+                </div>
+                <h1 className="note">
+                  Choosing Learn stack for your tech education means embarking
+                  on a transformation learning journey tailored to modern
+                </h1>
+                <div className="last">
+                  <div className="duration">
+                    <i class="fa-regular fa-clock font-a"></i>
+                    <div className="weeks">2 Weeks</div>
+                  </div>
+                  <div className="lesson">
+                    <i class="fa-regular fa-rectangle-list font-b"></i>
+                    <h1 className="fifteen">15 Lesson</h1>
+                  </div>
+
+                  <button className="btn">Enroll Now</button>
+                </div>
+              </div>
+            </div>
+            <div className="second-c">
+              <div className="top1">
+                <div className="amount">
+                  <h1>₦50,000</h1>
+                </div>
+              </div>
+
+              <div className="bottom1">
+                <div className="writeupone">
+                  <div>
+                    <h1>Front-End development</h1>
+                  </div>
+                  <div className="star">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star differ"></i>
+                  </div>
+                </div>
+                <h1 className="note">
+                  Choosing Learn stack for your tech education means embarking
+                  on a transformation learning journey tailored to modern
+                </h1>
+                <div className="last">
+                  <div className="duration">
+                    <i class="fa-regular fa-clock font-a"></i>
+                    <div className="weeks">2 Weeks</div>
+                  </div>
+                  <div className="lesson">
+                    <i class="fa-regular fa-rectangle-list font-b"></i>
+                    <h1 className="fifteen">15 Lesson</h1>
+                  </div>
+
+                  <button className="btn">Enroll Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </p>
- 
     </>
   );
 }
